aaebfb829e46981a8f913274d4ec8eec29774478
IBNESSAYEH <abdellatifibnessayeh@gmail.com>
2024-01-10 22:14:17 +0100

    add AddWikiModal layout and icons in home page

--- app/controllers/WikiController.php
+++ app/controllers/WikiController.php
@@ -5,6 +5,7 @@ namespace App\controllers;
 use App\models\Wiki;
 use App\models\Categorie;
 use App\models\Tag;
+use App\models\WikiTag;
 use App\models\User;
 use Core\Controller;
 class WikiController extends Controller
@@ -13,11 +14,14 @@ class WikiController extends Controller
     private $wiki;
     private $tags;
     private $categorie;
+    private $wiki_tag;
+
 
     public function __construct(){
         $this->wiki = new Wiki();
         $this->categorie = new Categorie();
         $this->tags = new Tag();
+        $this->wiki_tag = new WikiTag();
     }
     public function create(){
     if ($_SERVER["REQUEST_METHOD"] == "POST") {
@@ -27,7 +31,9 @@ class WikiController extends Controller
         $this->wiki->setIdCategorie($_POST['categorie']) ;
         $this->wiki->setIdUser($_SESSION['id_user']) ;
         $id_wiki = $this->wiki->createWiki();
-         var_dump($id_wiki) ;
+       foreach($_POST['tag'] as $tag){
+        $this->wiki_tag->createWikiTag($id_wiki, $tag);
+       }
     }
 
     return false;
@@ -44,5 +50,7 @@ class WikiController extends Controller
         $idWiki = intval(htmlspecialchars($_POST["id"])) ;
         $this->wiki->setId($idWiki);
         $this->wiki->UpdateWiki();
+        header("Location: {$_SERVER['HTTP_REFERER']}");
+exit;
     }
 }
